d2b3b8882618519a1902e4bdc05d281802221c79
Alumno <Alumno@LAB1507-02>
2024-08-20 17:26:00 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Persona
+    {
+        public Persona(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+        public Persona()
+        {
+
+        }
+
+        public string Name{ get; set; }
+        public string LastName{ get; set; }
+        public string NombresCompletos { get; set; }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Name + " " + LastName;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo = new Rectangulo();
+
+            Persona persona = new Persona{
+                Name = "Duberly",
+                LastName = "Dubery",
+            };
+            rectangulo.Base = 10;
+            rectangulo.Altura = 20;
+
+            persona.Name = "Duberly";
+            persona.LastName = "Mondragón";
+
+            persona.AsignarNombresCompletos();
+            Console.WriteLine(persona.NombresCompletos);
+
+            Console.Read();
+        }
+    }
+}
